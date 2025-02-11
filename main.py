a278fe64fb11a926931ceae7ec82b7e518eb093f
binaryCoder-101 <syed.misak@gmail.com>
2025-02-11 11:00:31 +0530

    Added functionality to count number of words and characters in main.py

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-print("hello world!")
+from collections import Counter
 
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-    print(file_contents)
+    book_path = "books/frankenstein.txt"
+    book_text = get_book_text(book_path)
+    num_words = get_num_words(book_text)
+    num_chars = get_characters_count(book_text)
+    print(f"{num_words} number of words found in the document")
+    print(num_chars)
 
+def get_book_text(path):
+    with open(path) as f:
+        book_text = f.read()
+    return book_text
+
+def get_num_words(text):
+    words = text.split()
+    num_words = len(words)
+    return num_words
+
+def get_characters_count(text):
+    lowered_text = text.lower()
+    characters_count_object = Counter(lowered_text)
+    characters_count = dict(characters_count_object)
+    return characters_count
+    
 main()
